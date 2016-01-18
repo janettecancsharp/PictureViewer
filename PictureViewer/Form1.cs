c30a47f178bc86a4af991b378da3d351f4274860
Janette Griggs <janette.can.csharp@gmail.com>
2016-01-18 09:42:09 -0800

    Name Button controls

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -16,5 +16,30 @@ namespace PictureViewer
         {
             InitializeComponent();
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
